3267abb169fc44533da2ad285a6d910b2534a6cf
Fred <ancheta86@gmail.com>
2019-12-10 23:37:10 -0800

    Upgrades

--- bot.py
+++ bot.py
@@ -7,6 +7,16 @@ import pytz
 
 app = Flask(__name__)
 
+# GROUP_ID
+# BOT_ID
+# GIPHY_RATING
+# GIPHY_TOKEN
+# NSFW_URL
+# NSFW_ALLOWED
+# NSFW_ALLOWED_ALL
+# CUSTOM_COMMAND
+# CUSTOM_URL
+
 def verify_groupme_message(json_body):
     return json_body['group_id'] == os.getenv('GROUP_ID') and json_body['sender_type'] != 'bot'
 
@@ -35,6 +45,7 @@ def get_giphy_url(query=''):
     giphy_token = os.getenv('GIPHY_TOKEN')
     giphy_rating = get_rating()
     giphy_url = "http://api.giphy.com/v1/gifs/search?q='%s'&api_key=%s&rating=%s&limit=100" % (query, giphy_token, giphy_rating)
+    print(giphy_url)
     response = requests.get(giphy_url)
     results = json.loads(response.text)
 
@@ -42,26 +53,27 @@ def get_giphy_url(query=''):
     return gif['images']['original']['url']
 
 def get_nsfw_url():
-    env_nsfw_url = os.getenv('NSFW_URL')
-    if env_nsfw_url:
-        nsfw_url = env_nsfw_url
-    else:
-        nsfw_url = "https://www.reddit.com/r/NSFW_GIF.json"
+    return os.getenv('NSFW_URL') or "https://www.reddit.com/r/FoodPorn.json"
 
-    response = requests.get(nsfw_url, headers={'User-agent': 'giphy-bot'})
+def get_custom_url():
+    return os.getenv('CUSTOM_URL') or "https://www.reddit.com/r/FoodPorn.json"
+
+def get_reddit_url(url):
+    response = requests.get(url, headers={'User-agent': 'giphy-bot'})
     results = json.loads(response.text)
 
     gif = results['data']['children'][random.randint(0, 25)]
     return gif['data']['url']
 
-def nsfw_allowed(json_body):
-    env_nsfw = os.getenv('NSFW') == 'yes'
-    env_all_power = os.getenv('NSFW_ALL_POWER') == 'yes'
 
-    if not env_nsfw:
+def nsfw_allowed(json_body):
+    nsfw_allowed = os.getenv('NSFW_ALLOWED') == 'yes'
+    nsfw_allowed_all = os.getenv('NSFW_ALLOWED_ALL') == 'yes'
+
+    if not nsfw_allowed:
         return False
 
-    if not env_all_power and not json_body['name'] == 'Fred':
+    if not nsfw_allowed_all and not json_body['name'] == 'Fred':
         return False
 
     if json_body['name'] == 'Fred':
@@ -108,8 +120,19 @@ def groupme_callback():
 
             return response
 
-        if message_parts[0] == '/thermonuclear' and nsfw_allowed(json_body):
-            response = get_nsfw_url()
+        if message_parts[0] == '/itadakipussy':
+            response = get_giphy_url('blackpink')
+            reply_to_groupme(response)
+
+        if message_parts[0] == '/erection' and nsfw_allowed(json_body):
+            url = get_nsfw_url()
+            response = get_reddit_url(url)
+            reply_to_groupme(response)
+
+        custom_command = os.getenv('CUSTOM_COMMAND') or 'thermonuclear'
+        if message_parts[0] == '/{}'.format(custom_command):
+            url = get_custom_url()
+            response = get_reddit_url(url)
             reply_to_groupme(response)
 
 
@@ -117,5 +140,4 @@ def groupme_callback():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port, debug=True)
     app.run(host='0.0.0.0', port=port)
